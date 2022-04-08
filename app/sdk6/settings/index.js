4d1492c98e9ab7867fff6c41dfbcbafd086f09f9
200Tigersbloxed <45884377+200Tigersbloxed@users.noreply.github.com>
2022-04-08 14:31:42 -0400

    Upgrade to SDK6/6.1 / New Global Server
    + Upgraded WebServer, now uses port 2096
    + Upgraded from SDK 5 to SDK 6/6.1

---
+++ app/sdk6/settings/index.js
@@ -0,0 +1,27 @@
+function MainSettings(props) {
+  return (
+    <Page>
+      <Section
+        title={<Text bold align="center">WebSocket Settings</Text>}>
+        <TextInput
+          settingsKey="wsUriSettings"
+          label="WS Uri"
+        />
+        <TextInput
+          settingsKey="serverPassSettings"
+          label="Server Password"
+        >CHANGEME</TextInput>
+        <Toggle
+          settingsKey="PublicServer"
+          label="Public Server"
+        ></Toggle>
+        <TextInput
+          settingsKey="PublicServerCode"
+          label="Public Server Code"
+        >CHANGEME</TextInput>
+      </Section>
+    </Page>
+  );
+}
+
+registerSettingsPage(MainSettings);
